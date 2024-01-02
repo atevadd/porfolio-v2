f0f390669f7b7130206d56e9993d92a285609f36
Adeniyi Tofunmi <adeniyitofunmi@gmail.com>
2024-01-02 10:11:10 +0100

    initial commit

---
+++ components/AppNavbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="atevadd-nav">
+    <div class="atevadd-nav__container">
+      <div class="atevadd-nav__logo">
+        <RouterLink to="/">TA</RouterLink>
+      </div>
+      <nav class="atevadd-nav__links">
+        <RouterLink to="/">About</RouterLink>
+        <RouterLink to="/">Porfolio</RouterLink>
+        <RouterLink to="/">Tools</RouterLink>
+        <RouterLink to="/">Contact</RouterLink>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script setup></script>
+
+<style lang="scss" src="../assets/scss/_navbar.scss" scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <AppNavbar />
+  <section class="atevadd-hero">
+    <div class="atevadd-hero__container">
+      <div class="atevadd-hero__text">
+        <h2>Hi, I'm Tofunmi</h2>
+        <h1>Frontend Developer</h1>
+      </div>
+      <div class="atevadd-hero__img">
+        <img src="@/assets/images/portrait.jpg" alt="" />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+import AppNavbar from "../components/AppNavbar.vue";
+</script>
+
+<style lang="scss" scoped></style>
